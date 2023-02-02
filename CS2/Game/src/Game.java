a366ddd00dae35e8a1145427cb53800f76a985c7
Fei C <alexbpontello@gmail.com>
2023-02-02 10:49:33 -0500

    hw

---
+++ CS2/Game/src/Game.java
@@ -0,0 +1,179 @@
+/* Johnny Knights
+ * Feifan Cao
+ * COP3503 Spring 2023
+ * Programming Assignment 1
+ */
+
+import java.io.File; // Import the File class
+import java.io.FileNotFoundException; // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class Game {
+	int[][] board;
+	char[] moves;
+	int boardSize;
+	int movesCount = 100;
+	int x, y;
+
+	public void print() {
+		for (int i = 0; i < boardSize; i++) {
+			for (int l = 0; l < boardSize; l++) {
+				System.out.print(board[i][l] + " ");
+			}
+			System.out.print("\n");
+		}
+	}
+
+	public Game(int boardSize, String fileName) {
+		this.boardSize = boardSize;
+		board = new int[boardSize][boardSize];
+		moves = new char[movesCount];
+		readMoves(fileName);
+	}
+
+	public void correctMoves1(char move, int winner) {
+		if (board[boardSize - 1][boardSize - 1] != 0)
+			return;
+
+		if (winner == 1) { // winner is 1
+			if (x + 2 < boardSize - 1 && y + 2 < boardSize - 1) {
+				board[++y][++x] = 1;
+			} else if (y + 1 == boardSize - 1) {
+				if (x % 2 == 1) { // currently odd
+					board[++y][x] = 1;
+				} else { // currently even
+					board[++y][++x] = 1;
+				}
+			} else if (x + 1 == boardSize - 1) {
+				if (y % 2 == 1) { // currently odd
+					board[y][++x] = 1;
+				} else { // currently even
+					board[++y][++x] = 1;
+				}
+			} else {
+				if (x + 1 < boardSize && y + 1 < boardSize) {
+					if (boardSize - x < boardSize - y)
+						board[y][++x] = 1;
+					else if (boardSize - x > boardSize - y)
+						board[++y][x] = 1;
+					else
+						board[++y][x] = 1;
+				} else if (x + 1 < boardSize)
+					board[y][++x] = 1;
+				else if (y + 1 < boardSize)
+					board[++y][x] = 1;
+				else {
+					System.out.println("check here x: " + x + " and y: " + y);
+				}
+			}
+		} else { // winner is 2
+			if (x + 2 < boardSize - 1 && y + 2 < boardSize - 1) {
+				board[++y][++x] = 1;
+			} else if (y + 1 == boardSize - 1) { // close to left side
+				if (x % 2 == 1) { // currently odd
+					if (x + 1 < boardSize)
+						board[++y][++x] = 1;
+					else
+						System.out.println("check here x: " + x + " and y: " + y);
+				} else { // currently even
+					board[++y][x] = 1;
+				}
+			} else if (x + 1 == boardSize - 1) { // close to right side
+				if (y % 2 == 1) { // currently odd
+					if (y + 1 < boardSize)
+						board[++y][++x] = 1;
+					else
+						board[y][++x] = 1;
+				} else { // currently even
+					board[y][++x] = 1;
+				}
+			} else {
+				if (x + 1 < boardSize - 1 && y + 1 < boardSize - 1) {
+					if (x % 2 == 0)
+						board[y][++x] = 1;
+					else {
+						if (move == 'd')
+							board[++y][++x] = 1;
+						else {
+							board[++y][x] = 1;
+						}
+					}
+				} else if (x + 1 < boardSize)
+					board[y][++x] = 1;
+				else if (y + 1 < boardSize)
+					board[++y][x] = 1;
+				else {
+					System.out.println("check here x: " + x + " and y: " + y);
+				}
+			}
+		}
+	}
+
+	public int play(int winner) {
+		int j = 0;
+		x = 0;
+		y = 0;
+
+		for (int l = 0; l < boardSize; l++) {
+			for (int k = 0; k < boardSize; k++) {
+				board[l][k] = 0;
+			}
+		}
+
+		while (board[boardSize - 1][boardSize - 1] == 0) {
+			if (winner == 1) { // if winner is player 1 then he has to stay on the odd x index
+				correctMoves1(moves[j], 1);
+			} else { // if winner is player 2 then make sure player 1 has to stay on even index
+				correctMoves1(moves[j], 2);
+			}
+			correctMoves2(moves[j++]);
+		}
+
+//		print();
+		return board[boardSize - 1][boardSize - 1];
+	}
+
+	public void correctMoves2(char move) {
+		if (board[boardSize - 1][boardSize - 1] != 0)
+			return;
+
+		if (move == 'd') {
+			if (x + 1 < boardSize && y + 1 < boardSize) {
+				board[++y][++x] = 2;
+			} else if (x + 1 < boardSize)
+				board[y][++x] = 2;
+			else {
+				board[++y][x] = 2;
+			}
+		} else if (move == 'b') {
+			if (y + 1 < boardSize)
+				board[++y][x] = 2;
+			else
+				board[y][++x] = 2;
+		} else if (move == 'r') {
+			if (x + 1 < boardSize)
+				board[y][++x] = 2;
+			else
+				board[++y][x] = 2;
+		} else {
+
+		}
+	}
+
+	public void readMoves(String fileName) // storing the letters from moves files in an char array
+	{
+		int i = 0;
+		try {
+			File myFile = new File("/home/net/fe477093/cs2/Game/src/" + fileName);
+//			File myFile = new File("C:\\Users\\AlexB\\eclipse-workspace\\Game\\src\\" + fileName);
+			Scanner myReader = new Scanner(myFile);
+			while (myReader.hasNextLine()) {
+				String move = myReader.nextLine();
+				moves[i++] = move.charAt(0);
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred:wrong file name or file not found");
+		}
+	}
+}
