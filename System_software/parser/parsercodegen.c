56d60f3e8e4c0b12b50ff5702ec2a7310e844a03
Fei C <alexbpontello@gmail.com>
2023-03-09 11:58:36 -0500

    done hw3

--- System_software/parser/parsercodegen.c
+++ System_software/parser/parsercodegen.c
@@ -2,7 +2,7 @@
     Feifan Cao, Juan M Garcia
     Dr. Montagne
     COP3402 Spring
-    HW2 - Lexical Analyzer
+    HW3 - Tiny PL/0 compile
 */
 
 #include <ctype.h>
@@ -17,6 +17,7 @@
 #define JUSTBIG 1000 /*just a big number for the arrays size*/
 #define ERROR 999    /*error code*/
 #define MAX_SYMBOL_TABLE_SIZE 500
+#define CODE_SIZE 500
 
 typedef struct
 {
@@ -25,14 +26,27 @@ typedef struct
     int val;       // number
     int level;     // L level
     int addr;      // M address
+    int mark;      // To indicate unavailable or deleted
 } symbol;
+
+typedef struct    //output struct for printing op codes
+{
+    char op[4];
+    int r;
+    int m;
+    int l; 
+} text;     
+
+text tex[JUSTBIG];
 
 symbol symbol_table[MAX_SYMBOL_TABLE_SIZE];
 
 int si = 0;        // symbol table index
 int lineCount = 0; // line count for the input program
 int numVars = -1;
-int m = 3;
+int m = 3;    
+int ti = 0;   //index for op lines
+int l = 0;
 
 void print_symb_table();
 int symb_table_check(char *str);
@@ -41,7 +55,7 @@ int check_for_reserved(char *word);
 int DFS_id(char *c);
 int DFS_num(char *c);
 char *getNextToken(FILE *inf);
-int check_mulSymbol(char *c);
+int check_mulSymbol(char *c);  //checking :=
 void error_f(int list_num);
 int const_decla_f(FILE *inf);
 int expression_f(FILE *inf, char *token_str);
@@ -50,44 +64,59 @@ int condition_f(FILE *inf, char *token_str);
 int statement_f(FILE *inf, char *token_str);
 int term_f(FILE *inf, char *token_str);
 int var_decla_f(FILE *inf);
-void block_f(FILE *inf);
+int block_f(FILE *inf);
+void print_Emit();
+void emit(char op[4], int l, int m);   //function used to send different op codes to array
 
-/*main Program*/
 int main(int argc, char **argv)
 {
     FILE *inf;
 
-    // opening File
     inf = fopen(argv[1], "r");
     if (inf == NULL)
     {
-        printf("Error opening file!"); // checking file if it exists
+        printf("Error: <opening file>"); 
         exit(1);
     }
     else
     {
 
-        printf("Assembly Code:\n");
-        printf("Line    OP    L    M\n");
-        printf("  0    JMP    0    3\n");
-        block_f(inf);
-
-        char c = fgetc(inf);
-        if (c != '.')
+        if (block_f(inf) != -1)
         {
-            error_f(1);
-        }else{
-            printf("  %d    SYS    0    3\n", ++lineCount);
+            
+            if (fgetc(inf) != '.')
+            {
+                error_f(1);
+            }
+            print_Emit();
         }
-
-        printf("\nSymbol Table:\n");
-        printf("Kind | Name        | Value | Level | Address | Mark\n");
-        printf("---------------------------------------------------\n");
-        printf("   3 |        main |     0 |     0 |     3 |     0\n");
-        print_symb_table();
 
         fclose(inf);
     }
+}
+
+void print_Emit()
+{
+    printf("Assembly Code: \n");
+    printf("Line    OP    L    M\n");
+    printf("  0    JMP    0    3\n");
+    for (int i = 0; i < ti; i++)
+    {
+        if (strcmp(tex[i].op, "call") == 0 || strcmp(tex[i].op, "precedure") == 0 || strcmp(tex[i].op, "else") == 0)
+        {
+            printf("9 ");
+        }
+
+        printf("  %d    %s    %d    %d\n", lineCount++, tex[i].op, tex[i].l, tex[i].m);
+    }
+
+    printf("  %d    SYS    %d    %d\n", lineCount, l, m);
+
+    printf("\nSymbol Table:\n");
+    printf("Kind | Name        | Value | Level | Address | Mark\n");
+    printf("---------------------------------------------------\n");
+    printf("   3 |        main |     0 |     0 |     3 |     1\n");
+    print_symb_table();
 }
 
 void print_symb_table()
@@ -261,75 +290,101 @@ int check_mulSymbol(char *c)
 
 void error_f(int list_num)
 { // finish the error message follow the instruction
+    printf("Error: <");
     if (list_num == 1)
     {
-        printf("Program must end with period\n");
+        printf("Program must end with period>\n");
+        exit(1);
     }
     else if (list_num == 2)
     {
-        printf("const, var, and read keywords must be followed by identifier\n");
+        printf("const, var, and read keywords must be followed by identifier>\n");
+        exit(1);
     }
     else if (list_num == 3)
     {
-        printf("symbol name has already been declare\n");
+        printf("symbol name has already been declared>\n");
+        exit(1);
     }
     else if (list_num == 4)
     {
-        printf("constants must be assigned with =\n");
+        printf("constants must be assigned with =>\n");
+        exit(1);
     }
     else if (list_num == 5)
     {
-        printf("constants must be assigned an integer value\n");
+        printf("constants must be assigned an integer value>\n");
+        exit(1);
     }
     else if (list_num == 6)
     {
-        printf("constant and variable declarations must be followed by a semicolon\n");
+        printf("constant and variable declarations must be followed by a semicolon>\n");
+        exit(1);
     }
     else if (list_num == 7)
     {
-        printf("undeclared identifier\n");
+        printf("undeclared identifier>\n");
+        exit(1);
     }
     else if (list_num == 8)
     {
-        printf("only variable values may be altered\n");
+        printf("only variable values may be altered>\n");
+        exit(1);
     }
     else if (list_num == 9)
     {
-        printf("assignment statements must use :=\n");
+        printf("assignment statements must use :=>\n");
+        exit(1);
     }
     else if (list_num == 10)
     {
-        printf("begin must be followed by end\n");
+        printf("begin must be followed by end>\n");
+        exit(1);
     }
     else if (list_num == 11)
     {
-        printf("if must be followed by then\n");
+        printf("if must be followed by then>\n");
+        exit(1);
     }
     else if (list_num == 12)
     {
-        printf("while must be followed by do\n");
+        printf("while must be followed by do>\n");
+        exit(1);
     }
     else if (list_num == 13)
     {
-        printf("condition must contain comparison operator\n");
+        printf("condition must contain comparison operator>\n");
+        exit(1);
     }
     else if (list_num == 14)
     {
-        printf("right parenthesis must follow left parenthesis\n");
+        printf("right parenthesis must follow left parenthesis>\n");
+        exit(1);
     }
     else if (list_num == 15)
     {
-        printf("arithmetic equations must contain operands, parentheses, numbers, or symbols\n");
+        printf("arithmetic equations must contain operands, parentheses, numbers, or symbols>\n");
+        exit(1);
     }
     else
     {
+        //printf("No error found");
     }
+}
+
+void emit(char op[4], int l, int m)
+{
+
+    // printf(" %s \n",op);
+    strcpy(tex[ti].op, op); // opcode
+    tex[ti].r = 0;          // register    not used in your project
+    tex[ti].l = l;          // lexicographical level
+    tex[ti].m = m;          // modifier
+    ti++;
 }
 
 int const_decla_f(FILE *inf)
 {
-    char c;
-    int i = 0;
     char *cur_read_str;
 
     do
@@ -341,6 +396,7 @@ int const_decla_f(FILE *inf)
             error_f(2);
             return -1;
         }
+
         if (symb_table_check(cur_read_str) != -1)
         {
             error_f(3);
@@ -349,34 +405,17 @@ int const_decla_f(FILE *inf)
 
         symbol_table[si].kind = 1;
         strcpy(symbol_table[si].name, cur_read_str);
+        cur_read_str = getNextToken(inf);
 
-        if (c == ' ')
-            while ((c = fgetc(inf)) == ' ')
-                ;
-
-        if (DFS_specialCh(c) != 9)
+        if (DFS_specialCh(cur_read_str[0]) != 9)
         {
             error_f(4);
             return -1;
         }
 
-        if ((c = fgetc(inf)) == ' ')
-            while ((c = fgetc(inf)) == ' ')
-                ;
-
-        i = 0;
-
-        if (isdigit(c))
+        cur_read_str = getNextToken(inf);
+        if (DFS_num(cur_read_str))
         {
-            cur_read_str[i++] = c;
-            cur_read_str = getNextToken(inf);
-
-            if (DFS_num(cur_read_str) < 0)
-            {
-                error_f(5);
-                return -1;
-            }
-
             symbol_table[si].val = atoi(cur_read_str);
             symbol_table[si].addr = 0;
             symbol_table[si].level = 0;
@@ -384,16 +423,15 @@ int const_decla_f(FILE *inf)
         }
         else
         {
+
             error_f(5);
             return -1;
         }
+        fseek(inf, -1, SEEK_CUR);
+        cur_read_str = getNextToken(inf);
+    } while (strcmp(cur_read_str, ",") == 0);
 
-        if (c == ' ')
-            while ((c = fgetc(inf)) == ' ')
-                ;
-    } while (c == ',');
-
-    if (c != ';')
+    if (strcmp(cur_read_str, ";") != 0)
     {
         error_f(6);
         return -1;
@@ -416,7 +454,7 @@ int expression_f(FILE *inf, char *token_str)
 
         term_f(inf, cur_read_str);
 
-        printf("  %d    NEG    0    %d\n", ++lineCount, m);
+        emit("NEG", l, m);
 
         while (strcmp(cur_read_str, "+") == 0 || strcmp(cur_read_str, "-") == 0)
         {
@@ -426,7 +464,7 @@ int expression_f(FILE *inf, char *token_str)
 
                 term_f(inf, cur_read_str);
 
-                printf("  %d    ADD    0    %d\n", ++lineCount, m);
+                emit("ADD", l, m);
             }
             else
             {
@@ -434,7 +472,7 @@ int expression_f(FILE *inf, char *token_str)
 
                 term_f(inf, cur_read_str);
 
-                printf("  %d    SUB    0    %d\n", ++lineCount, m);
+                emit("SUB", l, m);
             }
         }
     }
@@ -453,7 +491,7 @@ int expression_f(FILE *inf, char *token_str)
 
                     term_f(inf, cur_read_str);
 
-                    printf("  %d    ADD    0    %d\n", ++lineCount, m);
+                    emit("ADD", l, m);
                 }
                 else
                 {
@@ -461,11 +499,12 @@ int expression_f(FILE *inf, char *token_str)
 
                     term_f(inf, cur_read_str);
 
-                    printf("  %d    SUB    0    %d\n", ++lineCount, m);
+                    emit("SUB", l, m);
                 }
             }
         }
     }
+    return 1;
 }
 
 int factor_f(FILE *inf, char *token_str)
@@ -480,19 +519,20 @@ int factor_f(FILE *inf, char *token_str)
         if (symbol_table[curTokenIndex].kind == 1)
         {
             m = symbol_table[curTokenIndex].val;
-            printf("  %d    LIT    0    %d\n", ++lineCount, m);
+            emit("LIT", l, m);
         }
         else
         {
             m = symbol_table[curTokenIndex].addr;
-            printf("  %d    LOD    0    %d\n", ++lineCount, m);
+            emit("LOD", l, m);
         }
     }
     else
     {
         if (DFS_num(token_str))
         {
-            printf("  %d    LIT    0    %s\n", ++lineCount, token_str);
+            m = atoi(token_str);
+            emit("LIT", l, m);
         }
         else if (strcmp(token_str, "(") == 0)
         {
@@ -512,6 +552,7 @@ int factor_f(FILE *inf, char *token_str)
             return -1;
         }
     }
+    return 1;
 }
 
 int condition_f(FILE *inf, char *token_str)
@@ -526,7 +567,7 @@ int condition_f(FILE *inf, char *token_str)
 
         expression_f(inf, cur_read_str);
 
-        printf("  %d    ODD    0    %d\n", lineCount, m);
+        emit("ODD", l, m);
     }
     else
     {
@@ -538,7 +579,7 @@ int condition_f(FILE *inf, char *token_str)
 
             expression_f(inf, cur_read_str);
 
-            printf("  %d    EQL    0    %d\n", lineCount, m);
+            emit("EQL", l, m);
         }
         else if (strcmp(token_str, "!=") == 0)
         {
@@ -546,7 +587,7 @@ int condition_f(FILE *inf, char *token_str)
 
             expression_f(inf, cur_read_str);
 
-            printf("  %d    NEQ    0    %d\n", lineCount, m);
+            emit("NEQ", l, m);
         }
         else if (strcmp(token_str, "<") == 0)
         {
@@ -554,7 +595,7 @@ int condition_f(FILE *inf, char *token_str)
 
             expression_f(inf, cur_read_str);
 
-            printf("  %d    LSS    0    %d\n", lineCount, m);
+            emit("LSS", l, m);
         }
         else if (strcmp(token_str, "<=") == 0)
         {
@@ -562,7 +603,7 @@ int condition_f(FILE *inf, char *token_str)
 
             expression_f(inf, cur_read_str);
 
-            printf("  %d    LEQ    0    %d\n", lineCount, m);
+            emit("LEQ", l, m);
         }
         else if (strcmp(token_str, ">") == 0)
         {
@@ -570,7 +611,7 @@ int condition_f(FILE *inf, char *token_str)
 
             expression_f(inf, cur_read_str);
 
-            printf("  %d    GTR    0    %d\n", lineCount, m);
+            emit("GTR", l, m);
         }
         else if (strcmp(token_str, ">=") == 0)
         {
@@ -578,7 +619,7 @@ int condition_f(FILE *inf, char *token_str)
 
             expression_f(inf, cur_read_str);
 
-            printf("  %d    GEQ    0    %d\n", lineCount, m);
+            emit("GEQ", l, m);
         }
         else
         {
@@ -586,6 +627,8 @@ int condition_f(FILE *inf, char *token_str)
             return -1;
         }
     }
+
+    return 1;
 }
 
 int statement_f(FILE *inf, char *token_str)
@@ -614,7 +657,7 @@ int statement_f(FILE *inf, char *token_str)
 
             lineCount++;
             m = symbol_table[curTokenIndex].addr;
-            printf("  %d    STO    0    %d\n", lineCount, m);
+            emit("STO", l, m);
             return 1;
         }
         else
@@ -665,7 +708,7 @@ int statement_f(FILE *inf, char *token_str)
                 condition_f(inf, cur_read_str);
 
                 // int jpcIndex = current code Index
-                printf("  %d    JPC    0    %d\n", lineCount, m);
+                emit("JPC", l, m);
 
                 if (strcmp(cur_read_str, "then") == 0)
                 {
@@ -734,11 +777,11 @@ int statement_f(FILE *inf, char *token_str)
 
                 cur_read_str = getNextToken(inf);
 
-                printf("  %d    READ    0    %d\n", lineCount, m);
+                emit("READ", l, m);
 
                 m = symbol_table[symIdx].addr;
 
-                printf("  %d    STO    0    %d\n", lineCount, m);
+                emit("STO", l, m);
                 return 1;
             }
 
@@ -748,11 +791,12 @@ int statement_f(FILE *inf, char *token_str)
 
                 expression_f(inf, cur_read_str);
 
-                printf("  %d    WRITE    0    %d\n", lineCount, m);
+                emit("WRITE", l, m);
                 return 1;
             }
         }
     }
+    return 1;
 }
 
 int term_f(FILE *inf, char *token_str)
@@ -771,7 +815,8 @@ int term_f(FILE *inf, char *token_str)
         {
             cur_read_str = getNextToken(inf);
             factor_f(inf, cur_read_str);
-            printf("  %d    MUL    0    %d\n", ++lineCount, m);
+            m += 1;
+            emit("MUL", l, m);
         }
         else
         {
@@ -779,9 +824,10 @@ int term_f(FILE *inf, char *token_str)
 
             factor_f(inf, token_str);
 
-            printf("  %d    DIV    0    %d\n", ++lineCount, m);
+            emit("DIV", l, m);
         }
     }
+    return 1;
 }
 
 int var_decla_f(FILE *inf)
@@ -832,7 +878,7 @@ int var_decla_f(FILE *inf)
     return numVars;
 }
 
-void block_f(FILE *inf)
+int block_f(FILE *inf)
 {
     char *cur_read_str;
     char c;
@@ -843,16 +889,17 @@ void block_f(FILE *inf)
 
         if (isalpha(c))
         {
+
             fseek(inf, -1, SEEK_CUR);
             cur_read_str = getNextToken(inf);
 
             if (check_for_reserved(cur_read_str) == 28)
             {
+
                 if (const_decla_f(inf) < 0)
-                    return;
+                    return -1;
                 else
                 {
-                    lineCount++;
                 }
             }
             else if (check_for_reserved(cur_read_str) == 29)
@@ -861,13 +908,12 @@ void block_f(FILE *inf)
 
                 if (numVars < 0)
                 {
-                    return;
+                    return -1;
                 }
                 else
                 {
-                    lineCount++;
                     m += numVars;
-                    printf("  %d    INC    0    %d\n", lineCount, m);
+                    emit("INC", l, m);
                 }
             }
             else
@@ -875,7 +921,8 @@ void block_f(FILE *inf)
                 statement_f(inf, cur_read_str);
             }
         }
-        else if(c == '.') break;
+        else if (c == '.')
+            break;
         else
         {
             continue;
@@ -883,6 +930,7 @@ void block_f(FILE *inf)
     }
 
     free(cur_read_str);
-    fseek(inf, -1, SEEK_CUR);    
+    fseek(inf, -1, SEEK_CUR);
 
+    return 1;
 }
